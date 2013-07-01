5ca188057383d0c488552d16c833eda45d5b4e76
Stefan Richter <stefan.richter@figo.me>
2013-07-01 13:44:36 +0200

    Adding authenticated decorator, thanks to folz

--- pandora/pandora.py
+++ pandora/pandora.py
@@ -2,33 +2,40 @@ import urllib2
 
 from connection import PandoraConnection
 
+def authenticated(f):
+	def check_authentication(self, *args):
+		if not self.is_authenticated:
+			raise ValueError("User not yet authenticated")
+		return f(self, *args)
+	return check_authentication
+
 class Pandora(object):
 	station_id = None
-	authenticated = False
+	is_authenticated = False
 	backlog = []
 	
 	def __init__(self):
 		self.connection = PandoraConnection()
 	
 	def authenticate(self, username, password):
-		self.authenticated = self.connection.authenticate(username, password)
-		return self.authenticated
-		
+		self.is_authenticated = self.connection.authenticate(username, password)
+		return self.is_authenticated
+	
+	@authenticated
 	def get_station_list(self):
 		return self.connection.get_stations()
-	
+
+	@authenticated
 	def switch_station(self, station_id):
 		if type(station_id) is dict:
 			station_id = station_id['stationId']
-		
-		if not self.authenticated: raise ValueError("User not yet authenticated")
-		
+
 		self.backlog = []
 		self.station_id = station_id
 		self.backlog = self.connection.get_fragment(station_id) + self.backlog
-	
+
+	@authenticated
 	def get_next_song(self):
-		if not self.authenticated: raise ValueError("User not yet authenticated")
 		if not self.station_id: raise ValueError("No station selected")
 		
 		# get more songs
